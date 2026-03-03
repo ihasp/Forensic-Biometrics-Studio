de00ab34a1db4b676c687b8630f450acf1b47e14
Tomasz Matras <104440277+TomaszMatras@users.noreply.github.com>
2026-03-03 03:51:26 +0100

    fix: cleanup unnead files
    cleanedup fiels that shouldint have made it this far

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -160,26 +160,21 @@ fn main() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        })) 
         .invoke_handler(tauri::generate_handler![
             show_main_window_if_hidden,
             close_splashscreen_if_exists,
             open_settings_window,
             get_machine_id,
         ]);
-
-    #[cfg(not(target_os = "windows"))]
-    {
-        builder = builder.plugin(tauri_plugin_single_instance::init(
-            |app, argv, cwd| {
-                println!("{}, {argv:?}, {cwd}", app.package_info().name);
-                let _ = app
-                    .get_webview_window("main")
-                    .expect("no main window")
-                    .set_focus();
-            },
-        ));
-    }
 
     #[cfg(target_os = "windows")]
     {
